d271e761eba682224943b3ac6ce63a9e01309c6f
Battou <battou@MacBook-Pro-de-Camille.local>
2017-08-06 21:29:29 +0200

    Creating a Mozaik class

---
+++ main.rb
@@ -0,0 +1,64 @@
+require 'rubygems'
+require 'rmagick'
+require 'json'
+require_relative 'mozaik'
+include Magick
+
+if ARGV.empty?
+  	puts "No argument provided. Usage: ruby mozaik.rb [ filename ] "
+  	exit
+elsif ARGV.length > 1
+	puts "Too many arguments provided. Usage: ruby mozaik.rb [ filename ] "
+  	exit
+end
+
+ImgDetails = Struct.new(:name, :width, :height, :backgroundColor, :inputImg, :outputSize)
+
+
+# Read JSON from a file and extract parameters
+def read_input (file)
+	data_hash = JSON.parse(File.read(file))
+	name = data_hash["name"]
+	width = data_hash["width"]
+	height = data_hash["height"]
+	backgroundColor = data_hash["backgroundColor"]
+	inputImg = data_hash["inputImg"]
+	outputSize = data_hash["outputSize"]
+	return ImgDetails.new(name, width, height, backgroundColor, inputImg, outputSize)
+end
+
+# Read image from URL
+def read_url_image(adress)
+	return ImageList.new(adress).first
+end
+
+# Resize image
+def resize_image(image, width, height)
+	return image.resize_to_fill(width, height)
+end
+
+# Insert image into base frame
+def insert_image(baseImage, imageToInsert, x_position, y_position)
+	return baseImage.composite(imageToInsert, x_position, y_position, Magick::OverCompositeOp)
+end
+
+
+# Extract data from the input file
+inputData = read_input("input_info.json")
+
+# Create colored base for the final image
+finalImage = Image.new(inputData.width, inputData.height) { self.background_color = inputData.backgroundColor }
+
+# Read image from URL, resize and compose
+inputData.inputImg.each do |item|
+	imageToInsert = read_url_image(item["URL"])
+	factor = item["percentSize"].to_f/100
+	width = inputData.width*factor
+	height = inputData.height*factor
+	imageToInsert = resize_image(imageToInsert, width, height)
+	finalImage = insert_image(finalImage, imageToInsert, item["x_position"].to_i, item["y_position"].to_i)
+end
+
+# Print result
+finalImage.write(inputData.name)
+puts "This image is #{finalImage.columns}x#{finalImage.rows} pixels"
--- mozaik.rb
+++ mozaik.rb
@@ -1,62 +1,28 @@
 require 'rubygems'
 require 'rmagick'
-require 'json'
 include Magick
 
-if ARGV.empty?
-  	puts "No argument provided. Usage: ruby mozaik.rb [ filename ] "
-  	exit
-elsif ARGV.length > 1
-	puts "Too many arguments provided. Usage: ruby mozaik.rb [ filename ] "
-  	exit
+
+class Mozaik
+	def initialize (infos)
+		@name = infos["name"]
+		@width = infos["width"]
+		@height = infos["height"]
+		@baseFrame = Image.new(@width, @height) { self.background_color = infos["backgroundColor"] }
+		@mozaikParts = Array.new
+		@outputSize = infos["outputSize"]
+	end
+	def add_mosaic_parts(image)
+		@mozaikParts << image
+	end
+	def render_final_image
+		@mozaikParts.each do |mosaicPart|
+			image = ImageList.new(mosaicPart["URL"]).first
+			factor = mosaicPart["percentSize"].to_f/100
+ 			image.resize_to_fill!(@width*factor, @height*factor)
+			@baseFrame.composite!(image, mosaicPart["x_pos"].to_i, mosaicPart["y_pos"].to_i,  Magick::OverCompositeOp)
+		end
+		return @baseFrame
+	end
 end
 
-ImgDetails = Struct.new(:name, :width, :height, :backgroundColor, :inputImg, :outputSize)
-
-# Read JSON from a file and extract parameters
-def read_input (file)
-	data_hash = JSON.parse(File.read(file))
-	name = data_hash["name"]
-	width = data_hash["width"]
-	height = data_hash["height"]
-	backgroundColor = data_hash["backgroundColor"]
-	inputImg = data_hash["inputImg"]
-	outputSize = data_hash["outputSize"]
-	return ImgDetails.new(name, width, height, backgroundColor, inputImg, outputSize)
-end
-
-# Read image from URL
-def read_url_image(adress)
-	return ImageList.new(adress).first
-end
-
-# Resize image
-def resize_image(image, width, height)
-	return image.resize_to_fill(width, height)
-end
-
-# Insert image into base frame
-def insert_image(baseImage, imageToInsert, x_position, y_position)
-	return baseImage.composite(imageToInsert, x_position, y_position, Magick::OverCompositeOp)
-end
-
-
-# Extract data from the input file
-inputData = read_input("input_info.json")
-
-# Create colored base for the final image
-finalImage = Image.new(inputData.width, inputData.height) { self.background_color = inputData.backgroundColor }
-
-# Read image from URL, resize and compose
-inputData.inputImg.each do |item|
-	imageToInsert = read_url_image(item["URL"])
-	factor = item["percentSize"].to_f/100
-	width = inputData.width*factor
-	height = inputData.height*factor
-	imageToInsert = resize_image(imageToInsert, width, height)
-	finalImage = insert_image(finalImage, imageToInsert, item["x_position"].to_i, item["y_position"].to_i)
-end
-
-# Print result
-finalImage.write(inputData.name)
-puts "This image is #{finalImage.columns}x#{finalImage.rows} pixels"
