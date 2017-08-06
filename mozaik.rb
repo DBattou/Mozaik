94ca5a36e36c0448f484264e8afdecec3f85a612
Battou <battou@pc-109.home>
2017-08-06 15:32:13 +0200

    refactor, rewrite long lines, clean variables name

--- mozaik.rb
+++ mozaik.rb
@@ -6,23 +6,24 @@ include Magick
 # Read JSON from a file and extract parameters
 file = File.read("output_format.json")
 data_hash = JSON.parse(file)
-m_name = data_hash["name"]
-m_width = data_hash["width"]
-m_height = data_hash["height"]
-m_backgroundColor = data_hash["backgroundColor"]
-m_inputImg = data_hash["inputImg"]
-m_outputSize = data_hash["outputSize"]
+name = data_hash["name"]
+width = data_hash["width"]
+height = data_hash["height"]
+backgroundColor = data_hash["backgroundColor"]
+inputImg = data_hash["inputImg"]
+outputSize = data_hash["outputSize"]
 
 # Create colored base frame
-m_baseFrame = Image.new(m_width, m_height) { self.background_color = m_backgroundColor }
+baseFrame = Image.new(width, height) { self.background_color = backgroundColor }
 
 # Read image from URL, resize and compose
-m_inputImg.each do |item|
-	m_image = ImageList.new(item["URL"]).first
-	m_image.resize_to_fill!(m_width*(item["percentSize"].to_f/100), m_height*(item["percentSize"].to_f/100))
-	m_baseFrame.composite!(m_image, item["x_position"].to_i, item["y_position"].to_i, Magick::OverCompositeOp)
+inputImg.each do |item|
+	image = ImageList.new(item["URL"]).first
+	resize_factor = item["percentSize"].to_f/100
+	image.resize_to_fill!(width*resize_factor, height*resize_factor)
+	baseFrame.composite!(image, item["x_position"].to_i, item["y_position"].to_i, Magick::OverCompositeOp)
 end
 
 # Print result
-m_baseFrame.write(m_name)
-puts "This image is #{m_baseFrame.columns}x#{m_baseFrame.rows} pixels"
+baseFrame.write(name)
+puts "This image is #{baseFrame.columns}x#{baseFrame.rows} pixels"
